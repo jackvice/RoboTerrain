65e1e0a359915799017e8cbab7d1530a76fe91a9
jackvice <jack.vice@gmail.com>
2025-03-01 18:37:42 -0500

    leo rover update

--- ros2_ws/build/rover_metrics/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jack/src/RoboTerrain/ros2_ws/install/rover_metrics'
--- ros2_ws/build/roverrobotics_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jack/src/RoboTerrain/ros2_ws/src/roverrobotics_ros2/roverrobotics_description
-# Build directory: /home/jack/src/RoboTerrain/ros2_ws/build/roverrobotics_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
